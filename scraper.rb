01cac377049cd6eb7e0c912284f8c0e5026dca32
Oliver Denman <oliver@mysociety.org>
2017-05-10 17:18:06 +0100

    Remove table from database before scraping
    SqliteMagic creates a unique index at CREATE TABLE time. This means that if the unique index arguments of ScraperWiki.save_sqlite change, we need to create a new table to ensure the db reflects the change. Part of: https://github.com/everypolitician/everypolitician/issues/593

--- scraper.rb
+++ scraper.rb
@@ -29,5 +29,5 @@ def scrape_list(url)
   end
 end
 
-ScraperWiki.sqliteexecute('DELETE FROM data') rescue nil
+ScraperWiki.sqliteexecute('DROP TABLE data') rescue nil
 scrape_list('http://www.citizensforbahrain.com/index.php/entry/know-your-deputy-jamila-al-sammak-12th-northern')
